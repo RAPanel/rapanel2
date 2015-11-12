fad9620c153185f9b7975fcbf6ceca065f7f2cd3
semyonchick <semyonchick@gmail.com>
2015-11-12 20:28:50 +0500

    initial commit

--- components/RAComponent.php
+++ components/RAComponent.php
@@ -15,25 +15,80 @@ use Yii;
 use yii\base\BootstrapInterface;
 use yii\base\Component;
 use yii\helpers\ArrayHelper;
+use yii\web\HttpException;
 
 class RAComponent extends Component implements BootstrapInterface
 {
+    public $configList = ['core'];
+
     private $_modules;
+    private $_params;
 
     public function bootstrap($app)
     {
-        $list = Settings::find()->select(['path', 'value'])->asArray()->all();
-        $result = [];
-        foreach ($list as $row) {
-            $parse = explode('.', $row['path']);
-            $data = $row['value'];
-            foreach (array_reverse($parse) as $val) if ($val = trim($val))
-                $data = array($val => $data);
-            $result = ArrayHelper::merge($result, $data);
-        }
-        Yii::$app->params = ArrayHelper::merge(Yii::$app->params, $result);
+        // Include clever debug module
+        $this->debug();
+
+        // Configure app
+        $this->configure('default', false);
+        foreach ($this->configList as $config)
+            $this->configure($config);
+
+        // Import params
+        Yii::$app->params = $this->getParams();
 
         return true;
+    }
+
+    public function debug($debug = true)
+    {
+        if (in_array(php_uname('n'), ['localhost', 'devhost'])) {
+            $debug = true;
+            defined('YII_ENV') or define('YII_ENV', 'dev');
+            defined('YII_ENV_DEV') or define('YII_ENV_DEV', true);
+        }
+
+        if ($debug) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', true);
+            ini_set('display_startup_errors', true);
+            ini_set('html_errors', true);
+
+            defined('YII_DEBUG') or define('YII_DEBUG', true);
+        }
+    }
+
+    public function configure($name, $priority = true)
+    {
+        $file = Yii::getAlias('@ra/admin/config/' . $name . '.php');
+        if (!file_exists($file))
+            throw new HttpException(400, Yii::t('ra', 'Can`t find file "{file}"', ['file' => $file]));
+        $data = require(Yii::getAlias('@ra/admin/config/' . $name . '.php'));
+        foreach ($data as $key => $value) if (isset(Yii::$app->{$key})) {
+            if (!$priority && Yii::$app->{$key} || Yii::$app->{$key} == $value)
+                continue;
+            if (is_array(Yii::$app->{$key}) && is_array($value))
+                Yii::$app->$key = ArrayHelper::merge(Yii::$app->{$key}, $value);
+            else
+                Yii::$app->$key = $value;
+        }
+    }
+
+    public function getParams()
+    {
+        if ($this->_params === false) {
+            $this->_params = Yii::$app->params;
+            $list = Settings::find()->select(['path', 'value'])->asArray()->all();
+            foreach ($list as $row) {
+                $parse = explode('.', $row['path']);
+                $data = $row['value'];
+                foreach (array_reverse($parse) as $val)
+                    if ($val = trim($val))
+                        $data = array($val => $data);
+                $this->_params = ArrayHelper::merge($this->_params, $data);
+            }
+        }
+        return $this->_params;
     }
 
     public function getSettings()
---
+++ config/default.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: semyonchick
+ * Date: 02.09.2015
+ * Time: 16:22
+ */
+
+$email = 'no-reply@' . str_replace('www.', $_SERVER['HTTP_HOST'], $_SERVER['HTTP_HOST']);
+
+$config = [
+    'id' => 'rere',
+    'language' => 'en-US',
+    'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<c_:image>/_<type>/<name>' => '<c_>/index',
+                '<m_:rapanel>' => '<m_>/default/index',
+            ],
+        ],
+        'authManager' => [
+            'class' => 'ra\admin\components\AuthManager',
+        ],
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@webroot/theme/views',
+                    '@app/widgets/views' => '@webroot/theme/views/widgets',
+                ],
+                'baseUrl' => '@web/themes',
+            ],
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,
+            'messageConfig' => [
+                'from' => [$email],
+                'charset' => 'UTF-8',
+            ],
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'lVZD-kar4vfyeUGHVnVZbijmWiwgteuU',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => [
+        'adminEmail' => 'webmaster@rere-design.ru',
+        'fromEmail' => $email,
+    ],
+];
+
+if (YII_ENV_DEV) {
+    $allowedIPs = ['192.168.1.*', '78.159.225.99', '10.*.*.*'];
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => $allowedIPs,
+    ];
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => $allowedIPs,
+    ];
+}
+
+return $config;
