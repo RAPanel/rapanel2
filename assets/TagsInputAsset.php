6b8a57a637b218264be8c48fbbf2a17c35edbf9e
semyonchick <semyonchick@gmail.com>
2015-11-20 13:38:14 +0500

    initial commit

--- assets/TagsInputAsset.php
+++ assets/TagsInputAsset.php
@@ -10,7 +10,7 @@ class   TagsInputAsset extends AssetBundle
     public $sourcePath = '@bower/jquery-tags-input';
 
     public $css = [
-        'jquery.tagsinput.min.css',
+        'jquery.tagsinput.css',
     ];
 
     public $depends = [
