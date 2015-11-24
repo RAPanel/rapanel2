1d049518f02eec6f62fd8e14c89141d90817ee57
semyonchick <semyonchick@gmail.com>
2015-11-24 17:47:50 +0500

    initial commit

--- models/Form.php
+++ models/Form.php
@@ -74,12 +74,23 @@ class Form extends \yii\db\ActiveRecord
         return implode(PHP_EOL, $result);
     }
 
+    public function getSpamAttribute()
+    {
+        return 'controlUserNameExist';
+    }
+
+    /** for delete in futere */
+    public function contact($email = null)
+    {
+        return $this->send($email);
+    }
+
     /**
      * Sends an email to the specified email address using the information collected by this model.
      * @param  string $email the target email address
      * @return boolean whether the model passes validation
      */
-    public function contact($email = null, $cc = null)
+    public function send($email = null, $cc = null)
     {
         preg_match('#(\w+?)(?:Form)?$#iu', $this::className(), $matches);
         $this->type = lcfirst($matches[1]);
@@ -88,26 +99,38 @@ class Form extends \yii\db\ActiveRecord
             $mail = Yii::$app->mailer->compose();
             $mail->setTo(explode(',', $email ?: Yii::$app->params['adminEmail']));
             $mail->setFrom([Yii::$app->params['fromEmail'] => Yii::$app->name]);
-            $mail->setSubject((Yii::$app->request->post($this->spamAttribute) ? '[SPAM] ' : '') . 'Сообщение с сайта ' . Yii::$app->name);
+            $mail->setSubject(($this->getIsSpam() ? '[SPAM] ' : '') . 'Сообщение с сайта ' . Yii::$app->name);
             $mail->setTextBody($this->body);
 
             if (!empty($this->email))
                 $mail->setReplyTo($this->name ? [$this->email => $this->name] : $this->email);
 
-            if ($this->file) foreach ($this->file as $file)
-                $mail->attach($file->tempName, [
-                    'fileName' => $file->name,
-                    'contentType' => $file->type,
-                ]);
-
-            if ($cc && !Yii::$app->request->post($this->spamAttribute))
+            if ($cc && !$this->getIsSpam())
                 $mail->setCc(explode(',', $cc));
 
+            $this->beforeSend($mail);
+
             if ($mail->send() || YII_ENV_DEV) {
-                Yii::$app->session->setFlash('success', 'Сообщение успешно отправлено');
+                $this->afterSend($mail);
+
                 return true;
             }
         }
         return false;
+    }
+
+    public function getIsSpam()
+    {
+        return Yii::$app->request->post($this->spamAttribute);
+    }
+
+    public function beforeSend($mail)
+    {
+
+    }
+
+    public function afterSend($mail)
+    {
+        Yii::$app->session->setFlash('success', 'Сообщение успешно отправлено');
     }
 }
