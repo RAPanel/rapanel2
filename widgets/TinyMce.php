6a6853a19f55ab0e2940e2c4e8f32f26194617c0
semyonchick <semyonchick@gmail.com>
2015-12-23 08:34:40 +0500

    add spelcheck

--- widgets/TinyMce.php
+++ widgets/TinyMce.php
@@ -10,10 +10,12 @@ namespace ra\admin\widgets;
 
 
 use ra\admin\widgets\responsiveFilemanager\ResponsiveFilemanagerAsset;
+use yii\web\JsExpression;
 
 class TinyMce extends \dosamigos\tinymce\TinyMce
 {
     public $language = 'ru';
+    public $speller = true;
     public $fileManager = true;
     public $clientOptions = [
         'plugins' => [
@@ -23,9 +25,7 @@ class TinyMce extends \dosamigos\tinymce\TinyMce
             "emoticons template paste textcolor colorpicker textpattern autoresize",
         ],
         'extended_valid_elements'=>'script[type|src],iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder]',
-
         'toolbar1' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | forecolor backcolor | fullscreen",
-
     ];
 
     public function init()
@@ -39,6 +39,15 @@ class TinyMce extends \dosamigos\tinymce\TinyMce
             $this->clientOptions['external_plugins']['filemanager'] = $data->baseUrl . '/tinymce/plugins/responsivefilemanager/plugin.min.js';
             $this->clientOptions['external_plugins']['responsivefilemanager'] = $data->baseUrl . '/tinymce/plugins/responsivefilemanager/plugin.min.js';
             $this->clientOptions['toolbar1'] = 'responsivefilemanager ' . $this->clientOptions['toolbar1'];
+        }
+        if($this->speller){
+            $this->clientOptions['gecko_spellcheck'] = true;
+            $this->clientOptions['browser_spellcheck'] = true;
+            $this->clientOptions['plugins'][] = 'spellchecker';
+            $this->clientOptions['toolbar1'] .= ' | spellchecker';
+            $this->clientOptions['spellchecker_language'] = 'ru';
+            $this->clientOptions['spellchecker_rpc_url'] = 'http://speller.yandex.net/services/tinyspell';
+            $this->clientOptions['spellchecker_languages'] = 'Russian=ru,English=en,Ukrainian=uk';
         }
 
         parent::init();
