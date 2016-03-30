911fb670e2eb1ea42019ae1d0ec29a544140d0d2
semyonchick <semyonchick@gmail.com>
2016-03-30 14:04:36 +0500

    update to 2.0.8 compare

--- components/AuthManager.php
+++ components/AuthManager.php
@@ -338,4 +338,17 @@ class AuthManager implements ManagerInterface
     {
         // TODO: Implement removeAllAssignments() method.
     }
+
+    /**
+     * Checks the possibility of adding a child to parent
+     * @param Item $parent the parent item
+     * @param Item $child the child item to be added to the hierarchy
+     * @return boolean possibility of adding
+     *
+     * @since 2.0.8
+     */
+    public function canAddChild($parent, $child)
+    {
+        // TODO: Implement canAddChild() method.
+    }
 }
