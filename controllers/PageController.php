8787db065cb6c079517dd5c16ae4f9a38d4c100a
semyonchick <semyonchick@gmail.com>
2016-04-22 15:37:51 +0500

    Update meta properties

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -16,6 +16,8 @@ use yii\web\HttpException;
 
 class PageController extends Controller
 {
+    public $social = true;
+
     public function actionIndex($url = null)
     {
         if (is_null($url)) $url = "/$this->id";
@@ -34,26 +36,28 @@ class PageController extends Controller
         /** @var $base Page */
         if (isset($params['model'])) {
             $model = $params['model'];
-            if (method_exists($model, 'getData') && ($data = $model->data)) {
+            if (method_exists($model, 'getData') && ($data = $model->getData())) {
                 if (!empty($data['title'])) $this->getView()->title = $data['title'];
                 if (!Yii::$app->request->isAjax) {
-                    // Registry meta data
+                    // Registry meta seo data
                     if (!empty($data['description'])) $this->getView()->registerMetaTag(['name' => 'description', 'content' => $data['description']]);
                     if (!empty($data['keywords'])) $this->getView()->registerMetaTag(['name' => 'keywords', 'content' => $data['keywords']]);
                     // Registry og data
-                    $this->getView()->registerMetaTag(['property' => 'og:type', 'content' => 'website']);
-                    if (!empty($data['h1'])) $this->getView()->registerMetaTag(['property' => 'og:title', 'content' => $data['h1']]);
-                    if (method_exists($model, 'getHref')) $this->getView()->registerMetaTag(['property' => 'og:url', 'content' => $model->getHref(1, 1)]);
-                    if (method_exists($model, 'getPhoto') && $model->photo) {
-                        $this->getView()->registerMetaTag(['property' => 'og:image', 'content' => $model->photo->getHref('1000', true)]);
-                        $this->getView()->registerMetaTag(['property' => 'og:image:width', 'content' => $model->photo->getSizes('1000')['width']]);
-                        $this->getView()->registerMetaTag(['property' => 'og:image:height', 'content' => $model->photo->getSizes('1000')['height']]);
+                    if ($this->social) {
+                        $this->getView()->registerMetaTag(['property' => 'og:type', 'content' => 'website']);
+                        if (!empty($model['header'])) $this->getView()->registerMetaTag(['property' => 'og:title', 'content' => $model['header']]);
+                        if (!empty($model['about'])) $this->getView()->registerMetaTag(['property' => 'og:description', 'content' => $model['about']]);
+                        if (method_exists($model, 'getHref')) $this->getView()->registerMetaTag(['property' => 'og:url', 'content' => $model->getHref(1, 1)]);
+                        if (method_exists($model, 'getPhoto') && $model->photo) {
+                            $this->getView()->registerMetaTag(['property' => 'og:image', 'content' => $model->photo->getHref('1000', true)]);
+                            $this->getView()->registerMetaTag(['property' => 'og:image:width', 'content' => $model->photo->getSizes('1000')['width']]);
+                            $this->getView()->registerMetaTag(['property' => 'og:image:height', 'content' => $model->photo->getSizes('1000')['height']]);
+                        }
                     }
                 }
             }
-            $this->getView()->params['model'] = $params['base'] = $model;
-            $this->getView()->params['pageTitle'] = $model->name;
-            Yii::$app->view->params['active'] = [$model->id, $model->parent_id, $model->module_id];
+            $this->getView()->params['model'] = $model;
+            $this->getView()->params['active'] = [$model->id, $model->parent_id, $model->module_id];
             $this->getView()->params['breadcrumbs'] = function () use ($model) {
                 $result = [];
                 if ($model->parent_id) {
@@ -71,12 +75,19 @@ class PageController extends Controller
                         $query->andWhere(['>', 'parent_id', 0]);
                     $rows = $query->all();
                     if (isset($parent)) $rows[] = $parent;
-                    foreach ($rows as $row)
+                    foreach ($rows as $row) {
+                        $this->getView()->params['active'][] = $row['id'];
                         $result[] = ['label' => $row->name, 'url' => $row->href];
+                    }
                 }
                 $result[] = $model->name;
                 return $result;
             };
+            // @todo Для удаление в следующей версии
+            {
+                $params['base'] = $model;
+                $this->getView()->params['pageTitle'] = $model->name;
+            }
         }
         return parent::render($view, $params);
     }
@@ -89,7 +100,7 @@ class PageController extends Controller
     public function page($condition)
     {
         /** @var Page $class */
-        $page = Page::find()->joinWith(['pageData', 'photo'])->where($condition)->with('pageData')->one();
+        $page = Page::find()->where($condition)->with(['pageData', 'photo'])->one();
         if (!$condition || !$page) throw new HttpException(404, Yii::t('ra', 'Can`t find page'));
         return $page;
     }
