d7b5d344b6e0201dddfeeffd64288d9466006aee
semyonchick <semyonchick@gmail.com>
2016-05-12 11:21:59 +0500

    add key system

--- models/UserKey.php
+++ models/UserKey.php
@@ -4,6 +4,7 @@ namespace ra\admin\models;
 
 use Yii;
 use yii\db\Expression;
+use yii\web\HttpException;
 
 /**
  * This is the model class for table "{{%user_key}}".
@@ -20,12 +21,52 @@ use yii\db\Expression;
  */
 class UserKey extends \yii\db\ActiveRecord
 {
+    const TYPE_EMAIL_CHANGE = 1;
+    const TYPE_PASSWORD_CHANGE = 2;
+    const TYPE_PASSWORD_RESET = 3;
+    const TYPE_OTHER = 9;
+
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
         return '{{%user_key}}';
+    }
+
+    public static function generate($type, $userId, $expire = 60 * 5)
+    {
+
+        $model = new self();
+        $model->setAttributes([
+            'user_id' => $userId,
+            'type' => $type,
+            'key_value' => uniqid(uniqid()),
+            'create_time' => new Expression('now()'),
+            'expire_time' => new Expression('FROM_UNIXTIME(' . (time() + $expire) . ')'),
+        ]);
+        if ($model->save())
+            return $model->key_value;
+        else
+            throw new HttpException(400, print_r($model->errors, 1));
+
+        return false;
+    }
+
+    public static function add($type, $key, $expire)
+    {
+
+        $search = [
+            'type' => $type,
+            'key_value' => $key,
+        ];
+        $model = self::findOne($search);
+        if (!$model) $model = new self($search);
+        $model->setAttributes([
+            'user_id' => Yii::$app->user->id ?: ($model->user_id ? $model->user_id : 1),
+            'expire_time' => is_int($expire) ? new Expression('FROM_UNIXTIME(' . $expire . ')') : $expire,
+        ]);
+        return $model->save(false);
     }
 
     /**
@@ -72,23 +113,8 @@ class UserKey extends \yii\db\ActiveRecord
             [
                 'class' => '\yii\behaviors\TimestampBehavior',
                 'createdAtAttribute' => 'create_time',
+                'updatedAtAttribute' => false,
             ]
         ];
-    }
-
-    public static function add($type, $key, $expire){
-
-        $search = [
-            'type' => $type,
-            'key_value' => $key,
-        ];
-        $model = self::findOne($search);
-        if (!$model) $model = new self($search);
-        $model->setAttributes([
-            'user_id' => Yii::$app->user->id ?: ($model->user_id ? $model->user_id : 1),
-            'create_time' => new Expression('now()'),
-            'expire_time' => $expire,
-        ]);
-        return $model->save(false);
     }
 }
